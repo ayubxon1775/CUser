e8959771652b143bb56be82e3c4d7d7aa6494ba9
Ayubxon <ayubxonaxmatxonov@gmail.com>
2025-05-10 08:54:11 +0500

    added navbar and footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer-container container'>
+        <p>All right Reserved <a href="https://www.youtube.com/@akhrorweb">Akhror Soliyev</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='navbar-container container'>
+        <h1 className='navbar-logo'>CUser</h1>
+        <h3 className='navbar-counter'>No user :)</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
